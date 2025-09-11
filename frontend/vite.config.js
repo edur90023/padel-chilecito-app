03e05caaaac5a7f317035cb64d09b1f395943786
Eduardo Ricci <edur900@gmail.com>
2025-09-11 00:35:34 -0300

    cambios para PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,44 @@
+// frontend/vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Pádel Chilecito',
+        short_name: 'Pádel App',
+        description: 'Aplicación para la gestión de torneos de pádel en Chilecito.',
+        theme_color: '#111827',
+        background_color: '#111827',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
