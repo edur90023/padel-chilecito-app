1e2b4e19e8403503dfb1064494e866dea582f5f6
edur90023 <edur900@gmail.com>
2025-09-18 02:10:15 -0300

    Revert "feat: Implement full booking system with backend and frontend"

--- server/models/User.js
+++ server/models/User.js
@@ -10,11 +10,6 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'employee', 'client'],
-    default: 'client'
   }
 });
 
